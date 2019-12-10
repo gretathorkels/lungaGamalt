4cfc0a67cd93169e4f4ab567a0cd8be5e4133326
Diljá Þorkelsdóttir <dilja.thorkelsdottir@advania.is>
2019-12-10 22:52:00 +0000

    bætt við beinagrind

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
+
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navigation msg="Welcome to Your Vue.js App"/>
+    <Workshops msg="Welcome to Your Vue.js App"/>
+    <Events msg="Welcome to Your Vue.js App"/>
+    <FestivalProgram msg="Welcome to Your Vue.js App"/>
+   
   </div>
 </template>
 
+<!-- JAVASCRIPT -->
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Navigation from './components/Navigation.vue'
+import Workshops from './components/Workshops.vue'
+import Events from './components/Events.vue'
+import FestivalProgram from './components/FestivalProgram.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navigation,
+    Workshops, 
+    Events,
+    FestivalProgram
   }
 }
 </script>
 
+<!-- STÍLL CSS -->
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
