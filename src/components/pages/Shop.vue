694320246e9779668ef812c6ca2d1823b7454294
greta thorkelsdottir <greta.thorkels@gmail.com>
2020-07-13 21:10:39 +0000

    shop bætt við

---
+++ src/components/pages/Shop.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="about">
+      <Header v-bind:menu="'menu'" />
+      <Headline v-bind:title="title"/>
+
+      <div class="img-container">
+                                <img class="img-child" src="../../assets/tumblr/14-2018.jpg">
+                   </div>
+
+      <div class="text-container">
+          
+          <div class="sub-line">
+                {{$t("aboutlunga.firstline")}}
+           </div>
+          <div class="text-body">
+                {{$t("aboutlunga.para1")}}
+              </div>
+              <div class="text-body">
+              {{$t("aboutlunga.para2")}}
+              </div>
+          <div class="text-body">
+          {{$t("aboutlunga.para3")}}
+          </div>
+          <div class="text-body">
+              {{$t("aboutlunga.themeintro")}}
+          </div>
+          <div class="headline">
+              {{$t("aboutlunga.theme")}}
+          </div>
+          <div class="sub-line">
+              {{$t("aboutlunga.themeheadline")}}
+          </div>
+          <div class="text-body">
+              {{$t("aboutlunga.themepara1")}}
+          </div>
+          <div class="text-body">
+              {{$t("aboutlunga.themepara2")}}
+          </div>
+          <div class="text-body">
+              {{$t("aboutlunga.themepara3")}}
+          </div>
+          <div class="text-body">
+              {{$t("aboutlunga.themepara4")}}
+          </div>
+
+         <div class="sub-line">
+                        {{$t("aboutlunga.lastline")}}
+                   </div>
+
+      </div>
+      <div class="headline">
+      {{$t("aboutlunga.footer")}}
+</div>
+
+    <div class="img-container">
+        <img class="img-child" src="../../assets/tumblr/5.jpg">
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Header from '../shared/Header'
+import Headline from '../shared/Headline'
+
+export default {
+  name: 'About',
+  components: {
+      Header, 
+      Headline
+  },
+  data(){
+      return{
+          title: 'title.about'
+      }
+  }
+
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.headline {
+    text-align: center;
+}
+.sub-line{
+text-align:center;
+}
+
+.img-container {
+display:flex;
+flex-direction: column;
+flex-wrap:wrap;
+justify-content: center;
+align-self: center;
+
+}
+
+.img-child {
+max-width: 80%;
+  height: auto;
+  align-self: center;
+margin:20px;
+}
+
+
+</style>
