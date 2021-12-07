f8412d9803c68cb65ab3141e48e9c17397167d06
Greta Thorkels <greta.thorkels@gmail.com>
2021-12-07 14:06:15 +0200

    breytti verðinu

--- src/components/pages/Shop.vue
+++ src/components/pages/Shop.vue
@@ -21,7 +21,7 @@
           </div>
           </div>
         <div class="sub-line">
-          {{$t("shop.book")}}<br>€37
+          {{$t("shop.book")}}<br>€49
             <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
                 <input type="hidden" name="cmd" value="_s-xclick">
                 <input type="hidden" name="hosted_button_id" value="9WAJR53Z6W7P2">
